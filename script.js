0c86bef065633cf3ba7dc8c46a73ffd26ef469ed
Prakriti <127485457+adhprakriti@users.noreply.github.com>
2024-04-08 22:34:39 +0545

    Homepage(JavaScript)

---
+++ script.js
@@ -0,0 +1,35 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+}
+
+  function redirectToIndex() {
+    // Redirect the user to index.html
+    window.location.href = 'index.html';
+  }
+
+
